4f0e73221d6546a9b1e51c768077561c37ae2a7c
Richard Zijdeman <richard.zijdeman@gmail.com>
2017-07-13 19:56:33 +0100

    added required functions in NAMESPACE, properly thise time

--- grlcR/R/repo.summary.R
+++ grlcR/R/repo.summary.R
@@ -3,8 +3,8 @@
 #' This function retrieves the available sparql queries from a grlc repo
 #' @param a grlc repo owner/project
 #' @keywords grlc
-#' @import httr:content
-#' @import httr:GET
+#' @import httr::content
+#' @import httr::GET
 #' @export
 #' @examples
 #' repo.summary()
